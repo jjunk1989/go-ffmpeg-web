0ac986e152658884fb48888c5ccc4eef1f8bb1be
范承鑫 <fanchengxin1989@live.com>
2018-02-26 10:25:56 +0800

    move media

--- media.go
+++ media.go
@@ -1,4 +1,4 @@
-package media
+package main
 
 import (
 	"errors"
@@ -45,32 +45,6 @@ func (m Media) Run() {
 		"-b:a", m.Bitrate,
 		"-vf", m.VideoFormat,
 		m.Output)
-	out, err := c.CombinedOutput()
-	if err != nil {
-		fmt.Printf("run cmd error %s \r\n", err)
-	}
-	fmt.Printf("cmd out put: \r\n")
-	fmt.Printf("%s", out)
-}
-
-func testCmd() {
-	path, err := exec.LookPath("ffmpeg")
-	if err != nil {
-		fmt.Printf("look path err: %s \r\n", err)
-		return
-	}
-	fmt.Printf("find path is %s \r\n", path)
-	c := exec.Command("ffmpeg",
-		"-i", "test.gif",
-		"-t", "5",
-		"-ss", "00:00:00",
-		"-i", "test.mp3",
-		"-c:v", "libx264",
-		"-c:a", "aac",
-		"-b:a", "128k",
-		"-vf", "scale=420:-2,format=yuv420p",
-		"out.mp4")
-
 	out, err := c.CombinedOutput()
 	if err != nil {
 		fmt.Printf("run cmd error %s \r\n", err)
